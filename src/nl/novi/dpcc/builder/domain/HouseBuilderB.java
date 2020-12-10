470ca85bd767d0e7713164d0b8bd757e79c2e9a8
OzgurAbaci <o.abaci@novi-education.nl>
2020-12-10 16:05:57 +0100

    Nu wel opdracht b gemaakt

--- src/nl/novi/dpcc/builder/domain/HouseBuilderB.java
+++ src/nl/novi/dpcc/builder/domain/HouseBuilderB.java
@@ -1,36 +1,20 @@
 package nl.novi.dpcc.builder.domain;
 
 public class HouseBuilderB {
-    private String streetName;
-    private int housenumber;
-    private String houseNumberAddition;
-    private String postalCode;
+    private final String streetName;
+    private final int housenumber;
+    private final String houseNumberAddition;
+    private final String postalCode;
 
     //Niet verplicht
     private String province;
     private boolean hasGarden = false;
 
-    public HouseBuilderB(){
-    }
-
-    public HouseBuilderB withStreetname(String streetName){
+    public HouseBuilderB(String streetName, int housenumber, String houseNumberAddition, String postalCode) {
         this.streetName = streetName;
-        return this;
-    }
-
-    public HouseBuilderB withHousenumber(int housenumber){
         this.housenumber = housenumber;
-        return this;
-    }
-
-    public HouseBuilderB withHouseNumberAddition(String houseNumberAddition){
         this.houseNumberAddition = houseNumberAddition;
-        return this;
-    }
-
-    public HouseBuilderB withPostalCode(String postalCode){
         this.postalCode = postalCode;
-        return this;
     }
 
     public HouseBuilderB withProvince(String province){
@@ -45,22 +29,6 @@ public class HouseBuilderB {
 
     public House build(){
         return new House(streetName, housenumber, houseNumberAddition, postalCode, province, hasGarden);
-    }
-
-    public House buildWithEmptyCheck(){
-        if (streetName == null){
-            throw new RuntimeException("Streetname cannot be empty");
-        }
-        if (housenumber == 0){
-            throw new RuntimeException("House number cannot be empty");
-        }
-        if (houseNumberAddition == null){
-            throw new RuntimeException("House number addition cannot be empty. I know this makes no sense, but Nick told us so");
-        }
-        if (postalCode == null) {
-            throw new RuntimeException("Postal code cannot be empty");
-        }
-        return build();
     }
 
 }
